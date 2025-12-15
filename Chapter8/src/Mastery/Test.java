f489492a668db8923d22587896ddd8bd33b4c6ac
48051002 <48051002@L815WD220014.edu.cbe.ab.ca>
2025-12-15 12:53:43 -0700

    111

--- Chapter8/src/Mastery/Test.java
+++ Chapter8/src/Mastery/Test.java
@@ -1,52 +1,43 @@
 package Mastery;
-
 import java.util.Scanner;
-
 public class Test {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        UEmployee[] employees = new UEmployee[3];
-
-        for (int i = 0; i < 3; i++) {
-            System.out.println("\n--- Employee " + (i + 1) + " ---");
-
-            System.out.print("Employee name: ");
-            String name = in.nextLine();
-
-            System.out.print("Employee title (department or job title): ");
-            String titleOrDept = in.nextLine();
-
-            System.out.print("Employee salary: ");
-            double salary = in.nextDouble();
-            in.nextLine(); // clear newline
-
-            System.out.print("Type 1 for Faculty, 2 for Staff: ");
-            int choice = in.nextInt();
-            in.nextLine(); // clear newline
-
-            if (choice == 1) {
-                employees[i] = new Faculty(name, salary, titleOrDept);
-            } else {
-                employees[i] = new Staff(name, salary, titleOrDept);
-            }
-        }
-
-        System.out.println("\nUniversity Employees:");
-        for (UEmployee e : employees) {
-            if (e instanceof Faculty) {
-                Faculty f = (Faculty) e;
-                System.out.println(f.getName() + "  " +
-                                   f.getDepartment() + "  " +
-                                   f.getSalary());
-            } else if (e instanceof Staff) {
-                Staff s = (Staff) e;
-                System.out.println(s.getName() + "  " +
-                                   s.getJobTitle() + "  " +
-                                   s.getSalary());
-            }
-        }
-
-        in.close();
-    }
+   public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       UEmployee[] employees = new UEmployee[3];
+       for (int i = 0; i < employees.length; i++) {
+           System.out.println("\nEnter employee name");
+           String name = in.nextLine();
+           System.out.println("Enter employee salary");
+           double salary = in.nextDouble();
+           in.nextLine(); // clear leftover newline[web:14]
+           System.out.println("Are they faculty or staff? (1 = Faculty, 2 = Staff)");
+           int choice = in.nextInt();
+           in.nextLine(); // clear leftover newline
+           if (choice == 1) {
+               System.out.println("Enter Department/Faculty:");
+               String dept = in.nextLine();
+               employees[i] = new Faculty(name, salary, dept);
+           } else {
+               System.out.println("Enter employee title:");
+               String title = in.nextLine();
+               employees[i] = new Staff(name, salary, title);
+           }
+       }
+       System.out.println("\nUniversity Employees:");
+       for (UEmployee e : employees) {
+           if (e instanceof Faculty) {
+               Faculty f = (Faculty) e;
+               System.out.println(f.getName() + "  " +
+                                  f.getDepartment() + "  " +
+                                  f.getSalary());
+           } else if (e instanceof Staff) {
+               Staff s = (Staff) e;
+               System.out.println(s.getName() + "  " +
+                                  s.getJobTitle() + "  " +
+                                  s.getSalary());
+           }
+       }
+       in.close();
+   }
 }
+
